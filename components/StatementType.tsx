d53052f8a0ac251bd262b945d0c412931b3e7be9
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-09-21 21:47:36 +0100

    deploy fix

---
+++ components/StatementType.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Statements } from "@/models/statements/enum/Statements.enum";
+import {
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    SelectChangeEvent,
+} from "@mui/material";
+import React from "react";
+
+type StatementProps = {
+    onStatementChange(updatedStatement: Statements): void;
+};
+
+const StatementType = ({ onStatementChange }: StatementProps) => {
+    const statementKeys = Object.values(Statements);
+    const [statementType, setStatementType] = React.useState<Statements>(
+        Statements.NATWEST
+    );
+
+    const handleChange = (event: SelectChangeEvent) => {
+        console.log(statementType);
+        setStatementType(event.target.value as Statements);
+        onStatementChange(event.target.value as Statements);
+    };
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel
+                id="demo-simple-select-label"
+                sx={{
+                    color: "white",
+                    "&.Mui-focused.MuiInputLabel-root": {
+                        color: "white",
+                    },
+                }}
+            >
+                Bank
+            </InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={statementType}
+                label="Bank"
+                onChange={handleChange}
+                sx={{
+                    color: "white",
+                    ".MuiOutlinedInput-notchedOutline": {
+                        borderColor: "white",
+                    },
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "white",
+                    },
+                    "&:hover .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "white",
+                    },
+                    ".MuiSvgIcon-root ": {
+                        fill: "white !important",
+                    },
+                    "& .MuiFormLabel-root": {
+                        color: "white",
+                        paddingRight: "0.2rem",
+                        paddingLeft: "0.2rem",
+                    },
+                }}
+            >
+                {statementKeys.map((statementType, i) => {
+                    return (
+                        <MenuItem key={i} value={statementType}>
+                            {statementType}
+                        </MenuItem>
+                    );
+                })}
+            </Select>
+        </FormControl>
+    );
+};
+
+export default StatementType;
---
+++ components/Tags.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import React, { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import { TextField } from "@mui/material";
+
+export type Tag = {
+    description: string;
+    colour: string;
+};
+export interface TagsProps {
+    onTagsChange: (tags: Tag[]) => void;
+}
+
+export const Tags = ({ onTagsChange }: TagsProps) => {
+    const [tags, setTags] = useState<Tag[]>([]);
+    const [error, setError] = useState<string>("");
+
+    const colourOptions = ["bg-purple-300", "bg-purple-400", "bg-purple-500"];
+    const containsDuplicate = (word: string): boolean => {
+        return tags.some(tag => tag.description === word);
+    };
+    const handleAddTag = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        const description = (event.target as HTMLInputElement).value;
+        if (event.key === "Enter") {
+            if (containsDuplicate(description)) {
+                setError("Tag already exists");
+                return;
+            }
+            const newTag: Tag = {
+                description,
+                colour: colourOptions[
+                    Math.floor(Math.random() * colourOptions.length)
+                ],
+            };
+            const updatedTags = [...tags, newTag];
+            setTags(updatedTags);
+            onTagsChange(updatedTags);
+            setError(""); // Clear error if valid
+            (event.target as HTMLInputElement).value = "";
+        }
+    };
+
+    function handleRemoveTag(i: number): void {
+        const updatedTags = [...tags];
+        updatedTags.splice(i, 1);
+        setTags(updatedTags);
+        onTagsChange(updatedTags);
+    }
+
+    const createTag = (tag: Tag, i: number) => {
+        return (
+            <div
+                className={`${tag.colour} text-white rounded-3xl p-2 h-10 w-max flex justify-center items-center`}
+                key={i}
+            >
+                <p>{tag.description}</p>
+                <CloseIcon
+                    onClick={() => handleRemoveTag(i)}
+                    className="text-base text-white cursor-pointer hover:text-red-300"
+                />
+            </div>
+        );
+    };
+
+    return (
+        <>
+            <TextField
+                id="outlined-basic"
+                variant="outlined"
+                label="Tags"
+                sx={{
+                    "& .MuiOutlinedInput-root": {
+                        "& fieldset": {
+                            borderColor: "white",
+                        },
+                        "&:hover fieldset": {
+                            borderColor: "white",
+                        },
+                        "&.Mui-focused fieldset": {
+                            borderColor: "white",
+                        },
+                        "& input": {
+                            color: "white",
+                        },
+                    },
+                    "& .MuiInputLabel-root": {
+                        color: "white",
+                        paddingRight: "0.2rem",
+                        paddingLeft: "0.2rem",
+                    },
+                    "& .MuiInputLabel-root.Mui-focused": {
+                        color: "white",
+                    },
+                }}
+                onKeyDown={handleAddTag}
+                placeholder="Add key words to check"
+            />
+            <p className="text-red-700">{error}</p>
+
+            <div
+                className={`overflow-auto flex flex-wrap items-center justify-center gap-1 max-w-72 ${
+                    tags.length > 0 ? " md:max-h-64 max-h-24" : "max-h-10"
+                }`}
+            >
+                {tags.map((tag, i) => createTag(tag, i))}
+            </div>
+        </>
+    );
+};
